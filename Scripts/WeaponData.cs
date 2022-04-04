0aec42cafb05e302d297e875832846b5c306ce15
Alexandre <xand974@gmail.com>
2022-04-04 23:16:12 +0200

    enemy system

---
+++ Scripts/WeaponData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Game/Weapon Data")]
+public class WeaponData : ScriptableObject
+{
+    public int Damage;
+    public string Name;
+    public int FreezeDuration;
+    public GameObject Prefab;
+}
---
+++ Scripts/WeaponSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour
+{
+    [SerializeField] WeaponData weaponData;
+
+    [SerializeField] Transform playerHandTransform;
+
+    private  GameObject model;
+
+    private void OnEnable()
+    {
+        if (model != null) Destroy(model);
+        if(weaponData != null)
+        {
+            model = Instantiate(weaponData.Prefab);
+            model.transform.SetParent(playerHandTransform);
+            model.transform.localScale = new Vector3(x : 4 ,y :3 , z : 3);
+            model.transform.rotation = new Quaternion(x: 0.6179256f, y: 2.045542f, z: -0.16f , w : 5f);
+        }
+    }
+
+    void Update()
+    {
+        //Input for testing
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            Attack();
+        }
+    }
+
+    public void  Attack()
+    {
+        var target = GameObject.FindGameObjectWithTag("enemy").GetComponent<Target>();
+        if(weaponData.Damage > 0)
+        {
+            target.TakeDamage(weaponData.Damage);
+        }
+    }
+}
